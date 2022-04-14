644a16f036d29beaedae7005b96ddac03afef77e
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-14 21:14:32 +0200

    added functionality for all buttons except the random color button

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 const sliderInput = document.querySelector("#canvas-slider");
-const canvasGrid = document.querySelector('.canvas');
+const canvasGrid = document.querySelector(".canvas");
+const inputColor = document.querySelector("#color-picker");
+const colorBtn = document.querySelector(".color-mode");
+const randomColor = document.querySelector(".random-color");
+const eraserBtn = document.querySelector(".eraser");
+const clearBtn = document.querySelector(".clear");
 
-let = canvasSize = 0;
+let = currentSize = 32;
+let = currentColor = 0;
 
-// sliderInput.addEventListener(
-//   "change",
-//   function () {
-//     canvasSize = sliderInput.value;
-//     document.querySelector(".lbl-slider").textContent = `${canvasSize}x${canvasSize}`;
-//     makeGrid(canvasSize);
-//   },
-//   false
-// );
+sliderInput.addEventListener("input", (e) => {
+  document.querySelector(
+    ".lbl-slider"
+  ).textContent = `${e.target.value}x${e.target.value}`;
+  currentSize = e.target.value;
+  makeGrid(e.target.value);
+});
 
-sliderInput.addEventListener('input', e => {
-    document.querySelector('.lbl-slider').textContent = `${e.target.value}x${e.target.value}`
-    makeGrid(e.target.value);
-})
+inputColor.addEventListener("input", (e) => {
+  currentColor = e.target.value;
+});
+
+colorBtn.addEventListener("click", function () {
+  currentColor = document.querySelector("#color-picker").value;
+});
+
+randomColor.addEventListener("click", function () {});
+
+eraserBtn.addEventListener("click", function () {
+  currentColor = "white";
+});
+
+clearBtn.addEventListener("click", function () {
+  makeGrid(currentSize);
+});
 
 function makeGrid(size) {
-    canvasGrid.style.setProperty('--grid-rows', size);
-    canvasGrid.style.setProperty('--grid-cols', size);
-    canvasGrid.innerText = '';
-    for (c = 0; c < (size * size); c++) {
-        let cell = document.createElement("div");
-        cell.innerText = "";
-        canvasGrid.appendChild(cell).className = "grid-item";
-    };
-};
+  canvasGrid.style.setProperty("--grid-rows", size);
+  canvasGrid.style.setProperty("--grid-cols", size);
+  canvasGrid.innerText = "";
+  for (c = 0; c < size * size; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = "";
+    canvasGrid.appendChild(cell).className = "grid-item";
+  }
+}
 
 makeGrid(32);
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+gridItems.forEach((gridItem) => {
+  gridItem.addEventListener("click", () => {
+    gridItem.style.backgroundColor = `${currentColor}`;
+  });
+});
